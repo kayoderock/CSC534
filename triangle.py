df5a2f3e753ee0d1d7866ae682cc6bad82638e90
Kayode rock <kayoderock@gmail.com>
2017-01-29 23:12:17 +0100

    Initial setup

---
+++ triangle.py
@@ -0,0 +1,15 @@
+import turtle
+import math
+
+class Triangle :
+    def _ _init_ _(vertices):
+        self.vertices = vertices
+
+    def translate (newVertices):
+        
+
+        
+    def rotate (angle):
+
+
+    def draw ()
