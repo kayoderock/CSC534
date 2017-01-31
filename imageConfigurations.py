d36c654646e0f5a71cd08d1e7e7252415b6dc9e9
Kayode rock <kayoderock@gmail.com>
2017-01-31 23:00:11 +0100

    little debugg

---
+++ imageConfigurations.py
@@ -0,0 +1,4 @@
+import triangle
+
+TriangleCordinates = raw_input( "Please enter cordinates: " )
+myTriangle = triangle.Triangle(TriangleCordinates)
--- triangle.py
+++ triangle.py
@@ -1,15 +1,45 @@
 import turtle
 import math
 
+
 class Triangle :
-    def _ _init_ _(vertices):
+    def __init__(self,vertices):
         self.vertices = vertices
 
-    def translate (newVertices):
+    def translateCordinate(oldCordinate, transitionDistance):
+        cordinate = []
         
+        for i in range(2):
+            point = (1 - i)*(oldCordinate[0]) + i * oldCordinate[1] + transitionDistance[i] * (1)
+            cordinate.append(point)
 
+        return tuple(cordinate)
+
+    def translate (self, transitionDistance):
+        newPosition = []
+
+        for i in range(3):
+            newPosition.append(translateCordinate(selfVertices[i],transitionDistance))
+                    
+        self.vertices = newPosition;
         
-    def rotate (angle):
+    def __rotateCordinate__(oldCordinate, rotationAngle):
+        x = math.cos(rotationAngle)*(oldCordinate[0]) - math.sin(rotationAngle)*(oldCordinate[1])
+        y = math.sin(rotationAngle)*(oldCordinate[0]) + math.cos(rotationAngle)*(oldCordinate[1])
+        
+        return (x,y)
+    
+        
+    def rotate (self, angle):
+        newPosition = []
 
+        for i in range(3):
+            newPosition.append(rotateCordinate(selfVertices[i],angle))
+                    
+        self.vertices = newPosition;
+        
+        return self.vertices
 
-    def draw ()
+    
+    def draw ():
+        return null;
